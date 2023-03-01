e48e44258459acd9d2a6718c343dfe01d90599fd
kmeans27 <markusmueller27@protonmail.com>
2023-03-01 15:39:44 +0100

    added the classify_image() function to this file

--- batch_predict.py
+++ batch_predict.py
@@ -1,7 +1,8 @@
 import os
 import csv
 from datetime import datetime
-from flask_app import classify_image
+from tensorflow import keras
+import numpy as np
 
 # Set up paths and directories
 image_dir = 'new_images'
@@ -12,6 +13,26 @@ csv_path = os.path.join(output_dir, 'predictions.csv')
 
 # Set environment variable to use CPU only
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+# Load the saved model
+model = keras.models.load_model('ball_classifier.h5')
+train_dir = 'data/train'
+class_labels = sorted(os.listdir(train_dir))
+
+def classify_image(file_path):
+    # Make a prediction on the uploaded file
+    img = keras.utils.load_img(file_path, target_size=(224, 224))
+    img = keras.utils.img_to_array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)[0]
+    predicted_class_index = np.argmax(prediction)
+    predicted_class_label = class_labels[predicted_class_index]
+    return predicted_class_label
+
+
+
+
 
 # Get the list of image files
 image_files = [f for f in os.listdir(image_dir) if f.endswith('.jpg')]
